ac2250d036f85fc5b87ce8359e14bc615f0b524e
ChinoUkaegbu <chinoukaegbu@gmail.com>
2021-09-23 13:30:09 +0400

    Uploading html css and js

---
+++ script.js
@@ -0,0 +1,12 @@
+console.log("The program is running");
+
+var elem = document.getElementById("myvideo");
+function openFullscreen() {
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.webkitRequestFullscreen) { /* Safari */
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) { /* IE11 */
+    elem.msRequestFullscreen();
+  }
+}
