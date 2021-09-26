12ed68428d1d8912e232da87c41ea02f24e48777
ChinoUkaegbu <chinoukaegbu@gmail.com>
2021-09-26 15:48:24 +0400

    comments

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-console.log("The program is running");
+console.log("The program is running"); //Prints in the console to check if program is running in the first place
 
-var elem = document.getElementById("myvideo");
+var elem = document.getElementById("myvideo"); //lets js know that the element to be affected is the video
 function openFullscreen() {
   if (elem.requestFullscreen) {
     elem.requestFullscreen();
-  } else if (elem.webkitRequestFullscreen) { /* Safari */
+  } else if (elem.webkitRequestFullscreen) { /* Safari */ //for different browsers
     elem.webkitRequestFullscreen();
   } else if (elem.msRequestFullscreen) { /* IE11 */
     elem.msRequestFullscreen();
